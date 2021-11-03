590b446be64ae800fc546a4c30d2f1725767e81d
WojtAcht <wachtelik@gmail.com>
2021-11-03 14:43:42 +0100

    Handle different badges for android and aps notifications

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-push-notifications "1.0.3"
+(defproject clj-push-notifications "1.0.4"
   :description "Firebase cloud messaging client."
   :url "https://github.com/spinneyio/clj-push-notifications"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
